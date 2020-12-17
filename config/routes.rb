b1342d5b17ef42b89bfca94678e685e9ce0b80d8
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2020-12-17 18:57:21 +0900

    データの追加場所のビューの整え

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'cards#index' #トップページへ遷移
-  resources :cards, only: [:index, :show, :create, :edit, :delete]
+  resources :cards, only: [:index, :show, :create, :edit, :destroy]
 end
