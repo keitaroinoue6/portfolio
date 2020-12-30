1dd485748c673ab8d85d4ee13438d5da734e341a
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2020-12-30 22:30:49 +0900

    ancestryの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_021829) do
+ActiveRecord::Schema.define(version: 2020_12_29_021731) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "language"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_12_14_021829) do
     t.string "releasedate"
     t.text "author"
     t.text "memo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
