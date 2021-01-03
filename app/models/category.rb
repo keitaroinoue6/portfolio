e76c793758abe16d8fe337d1e4a0cf046948cae4
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-03 14:41:34 +0900

    カテゴリーコントローラーの追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
+  has_many :cards
   has_ancestry
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+  
+  resources :categories, only: :show
+
 end
