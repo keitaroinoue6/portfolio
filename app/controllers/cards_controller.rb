5f1bc05322ce0d38f2db33405c5a9ad487e6c227
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-07 17:15:23 +0900

    permitにカラムの追加

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,7 @@ class CardsController < ApplicationController
   before_action :set_card, only: [:edit, :show]
   #before_action :goback, only: [:update, :destroy]
   def index
-    @cards = Card.all#.page(params[:page]).per(6)
+    @cards = Card.all.page(params[:page]).per(6)
   end
 
   def new
@@ -42,7 +42,7 @@ class CardsController < ApplicationController
   private
 
   def cards_params
-    params.require(:card).permit(:language, :title, :releasedate, :price, :author, :memo)
+    params.require(:card).permit(:language, :title, :releasedate, :price, :author, :memo, :category_id)
   end
 
   def set_card
