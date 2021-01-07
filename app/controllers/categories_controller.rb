14e5ced5a847855d2a98c7e809d1142c57c09ab2
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-08 00:07:36 +0900

    ページネーションの日本語化

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,10 @@
 class CategoriesController < ApplicationController
   def show
     #@category = Category.where(id: params[:category_id])
-    #@items = @category.cards.order('created_at DESC')
     #@categories = Category.all
     #@category = Category.find(params[:id])
-    @cards = Card.where(language: "HTML")
+    @cards = Card.where(category_id: 2)
+    #@card = Card.find_by(language: )
+
   end
 end
