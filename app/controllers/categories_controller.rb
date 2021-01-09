1a8b57cff138b1289f4e774880661902035a19ea
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-09 22:17:15 +0900

    headerのpath指定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,10 @@ class CategoriesController < ApplicationController
   def show
     #@category = Category.where(id: params[:category_id])
     #@categories = Category.all
-    #@category = Category.find(params[:id])
+    @category = Category.find(params[:id])
     @cards = Card.where(category_id: params[:id])
     #@card = Card.find(params[:id])
+    @cards = Card.all.page(params[:page]).per(6)
 
   end
 end
