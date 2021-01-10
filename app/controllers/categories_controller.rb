12dd75a4a91f6e11f44231828a0304f5fbc15b88
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-10 11:32:19 +0900

    登録一覧にページネーション追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
     #@category = Category.where(id: params[:category_id])
     #@categories = Category.all
     @category = Category.find(params[:id])
-    @cards = Card.where(category_id: params[:id])
+    @cards = Card.where(category_id: params[:id]).page(params[:page]).per(7)
     #@card = Card.find(params[:id])
   end
 end
