54879a55a13b30c3840cb19d33f0066f1aae7616
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-19 22:31:34 +0900

    getElementsByTagNameに変更

--- app/assets/javascripts/hamburger.js
+++ app/assets/javascripts/hamburger.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementsByClassName("hamburger")[0].addEventListener('click', function() {
     this.classList.toggle("active");
-    document.getElementsByClassName("sub")[0].classList.toggle("active");
+    document.getElementsByTagName("nav")[0].classList.toggle("active");
   })
 });
