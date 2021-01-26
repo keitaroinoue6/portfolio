86b1dd6b9fe3ba6cc274ccdbfeeca82172b2869a
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-26 13:24:41 +0900

    initialcommit

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -1,18 +1,20 @@
-var mySwiper = new Swiper ('.swiper-container', {
-  loop: true,        
-  effect: 'slide',   
-  speed: 3000,       
-  autoplay: {        
-    delay: 3000,
-    disableOnInteraction: true
-  },
-  pagination: {      
-    el: '.swiper-pagination',
-    type: 'bullets', 
-    clickable: true 
-  },
-  navigation: {      
-    nextEl: '.swiper-button-next', 
-    prevEl: '.swiper-button-prev', 
-  },
+$(function() {
+  var mySwiper = new Swiper ('.Content__like__end__swiper-container', {
+    loop: true,        //画像のループ
+    effect: 'slide',   //切り替わるときのアニメーション
+    speed: 3000,       //画像の切替スピード
+    autoplay: {        //自動で動かす
+      delay: 3000,
+      disableOnInteraction: true
+    },
+    pagination: {      //ページネーション
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {      //ナビケーション
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 });
