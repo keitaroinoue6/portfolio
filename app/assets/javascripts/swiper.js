d52577b00bdc0ae050bee8801322f3dd05d98c98
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-27 10:15:58 +0900

    scssの変更

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -3,18 +3,18 @@ $(function() {
     loop: true,        //画像のループ
     effect: 'slide',   //切り替わるときのアニメーション
     speed: 3000,       //画像の切替スピード
-    autoplay: {        //自動で動かす
-      delay: 3000,
-      disableOnInteraction: true
-    },
+    // autoplay: {        //自動で動かす
+    //   delay: 3000,
+    //   disableOnInteraction: true
+    // },
     pagination: {      //ページネーション
-      el: '.swiper-pagination',
+      el: '.Content__like__end__swiper-pagination',
       type: 'bullets',
       clickable: true
     },
     navigation: {      //ナビケーション
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.Content__like__end__swiper-button-next',
+      prevEl: '.Content__like__end__swiper-button-prev',
     },
   });
 });
