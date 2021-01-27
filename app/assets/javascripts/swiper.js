7caaca808b3f18b9e1f32afc2d9ad5709b7043b2
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-27 15:13:56 +0900

    変数をvarからletに変更

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -1,5 +1,5 @@
 $(function() {
-  var mySwiper = new Swiper ('.Content__like__end__swiper-container', {
+  let mySwiper = new Swiper ('.swiper-container', {
     loop: true,        //画像のループ
     effect: 'slide',   //切り替わるときのアニメーション
     speed: 3000,       //画像の切替スピード
@@ -8,13 +8,13 @@ $(function() {
     //   disableOnInteraction: true
     // },
     pagination: {      //ページネーション
-      el: '.Content__like__end__swiper-pagination',
+      el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
     },
     navigation: {      //ナビケーション
-      nextEl: '.Content__like__end__swiper-button-next',
-      prevEl: '.Content__like__end__swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
   });
 });
