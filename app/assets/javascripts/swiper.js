034b760d09b986b294fbe7c0ea58ad34027296f9
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-01-28 15:17:12 +0900

    画像のレイアウトの整え

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -2,15 +2,16 @@ $(function() {
   let mySwiper = new Swiper ('.swiper-container', {
     loop: true,        //画像のループ
     effect: 'slide',   //切り替わるときのアニメーション
-    speed: 3000,       //画像の切替スピード
+    speed: 1000,       //画像の切替スピード
+    // centeredSlides: true, // 画像を中央に
     // autoplay: {        //自動で動かす
     //   delay: 3000,
     //   disableOnInteraction: true
     // },
     pagination: {      //ページネーション
       el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+      type: 'bullets', //ooo形式
+      clickable: true //'bullets'のときのみ有効oクリックで直接そのスライで移動
     },
     navigation: {      //ナビケーション
       nextEl: '.swiper-button-next',
