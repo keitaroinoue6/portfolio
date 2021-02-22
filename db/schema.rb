f96251de1bfe311b16ce05e9d95236f42ad70036
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-02-22 14:41:56 +0900

    usersテーブルにニックネームカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_215115) do
+ActiveRecord::Schema.define(version: 2021_02_22_054109) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "language"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_02_19_215115) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
