ea8d7cc1ccbc0c69b27bccbe8cfeb49ead17dd37
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-03-11 20:28:55 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true # 普段開発環境でみているお馴染みのエラー画面にする方法
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
