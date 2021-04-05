1dcbc9cccb595cf5dd463599765c359eb314f68d
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-04-05 15:09:55 +0900

    find_byに変更

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,8 @@ class CategoriesController < ApplicationController
   def show
     #@category = Category.where(id: params[:category_id])
     #@categories = Category.all
-    @category = Category.find(params[:id])
+    # @category = Category.find(params[:id])
+    @category = Category.find_by(card_id: params[:id])
     @cards = Card.where(category_id: params[:id]).page(params[:page]).per(7)
     #@card = Card.find(params[:id])
   end
