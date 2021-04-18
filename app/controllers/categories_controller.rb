f7841807f990d5370655393454a6cd709ae3b4ee
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-04-18 12:37:48 +0900

    categorycontrollerの変更

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    #@category = Category.where(id: params[:category_id])
-    #@categories = Category.all
-    # @category = Category.find(params[:id])
-    @category = Category.find_by(card_id: params[:id])
+    @category = Category.find_by(params[:id])
     @cards = Card.where(category_id: params[:id]).page(params[:page]).per(7)
-    #@card = Card.find(params[:id])
   end
 end
