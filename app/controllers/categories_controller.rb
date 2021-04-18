1f87b9c9bb56fe2bf3e6a8f47ca4b3b781188209
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-04-18 12:43:24 +0900

    commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find_by(params[:id])
+    @category = Category.find_by(card_id: params[:id])
     @cards = Card.where(category_id: params[:id]).page(params[:page]).per(7)
   end
 end
